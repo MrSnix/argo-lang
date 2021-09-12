82d85707867c4d3901a37538a6ac7fb1b36df9b8
G.B <baittiner.giuseppe.dev@gmail.com>
2021-09-12 23:53:28 +0200

    Added missing stack fields

--- src/vm/src/diagnostic/snapshots.c
+++ src/vm/src/diagnostic/snapshots.c
@@ -46,10 +46,18 @@ void vm__snaps_memory(VM *vm)
   printf("+========================================+\n");
   printf("| <STACK>%*s|\n", 32, "");
   printf("+========================================+\n");
+  printf("| %-14s %-12s %-10s |\n", "TYPE", "NAME", "VALUE");
+  printf("+----------------------------------------+\n");
+  printf("| %-14s %-12s %-10d |\n", "(int32_t)", "offset", vm->stack->offset);
+  printf("| %-14s %-12s %-10d |\n", "(int32_t)", "counter", vm->stack->counter);
+  printf("| %-14s %-12s %-10d |\n", "(int32_t)", "current", vm->stack->current);
+  printf("| %-14s %-12s %-10d |\n", "(int32_t)", "max", vm->stack->max);
+  printf("+----------------------------------------+\n");
 
   if (vm->stack->counter)
   {
-
+    printf("| <DATA>%*s|\n", 33, "");
+    printf("+----------------------------------------+\n");
     printf("| %-14s %-12s %-10s |\n", "TYPE", "ADDR", "VALUE");
     printf("+----------------------------------------+\n");
 
