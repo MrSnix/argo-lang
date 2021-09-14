19895e9a35abdf6b8259f2e6292528009fb33787
Giuseppe Baittiner <baittiner.giuseppe.dev@gmail.com>
2021-09-14 22:54:14 +0200

    Added sample program

--- src/assembler/src/main/java/Main.java
+++ src/assembler/src/main/java/Main.java
@@ -8,7 +8,27 @@ import org.antlr.v4.runtime.CommonTokenStream;
 public class Main {
 	public static void main(String[] args) {
 		// Create stream input
-		CharStream stream = CharStreams.fromString("");
+		CharStream stream = CharStreams.fromString("# The following .arg equals to this code:\n" +
+				"# \n" +
+				"# function sum(int a, int b) {\n" +
+				"#   return a + b;\n" +
+				"# }\n" +
+				"# function main() {\n" +
+				"#   return sum(5, 4);\n" +
+				"# }\n" +
+				"#\n" +
+				"\n" +
+				":sum {\n" +
+				"    ADD\n" +
+				"    RET\n" +
+				"}\n" +
+				"\n" +
+				":main {\n" +
+				"    PSH, 5 \n" +
+				"    PSH, 4 \n" +
+				"    CLL, sum \n" +
+				"    HLT\n" +
+				"}\n");
 		// Feed lexer
 		ArgoLexer lexer = new ArgoLexer(stream);
 		// Create intermediate representation
