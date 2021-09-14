7b68ad2a5e8cb1ccf281399b6753d8a933a0136e
Giuseppe Baittiner <baittiner.giuseppe.dev@gmail.com>
2021-09-15 00:36:20 +0200

    Added bytecode generator class

---
+++ src/assembler/src/main/java/bytecode/Generator.java
@@ -0,0 +1,61 @@
+package bytecode;
+
+import compiler.ArgoBaseVisitor;
+import compiler.ArgoParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Generator extends ArgoBaseVisitor<ClassFile> {
+
+	public static final int MAJOR_VERSION = 1;
+	public static final int MINOR_VERSION = 0;
+
+	private final ClassFile classFile;
+
+	public Generator() {
+		this.classFile = new ClassFile(MAJOR_VERSION, MINOR_VERSION);
+	}
+
+	@Override
+	public ClassFile visitInstruction(ArgoParser.InstructionContext ctx) {
+		// Get routine name
+		final String name = ((ArgoParser.RoutineContext) ctx.parent).label().name().getText();
+		// Get routine data
+		final Routine routine = this.classFile.getRoutinesTable().get(name);
+		// Get instruction name
+		final String instruction = ctx.opcode().getText();
+		// Parse opcode and insert
+		routine.getData().add(Opcode.mapToBytecode(instruction));
+		// Verify special case
+		if(Opcode.valueOf(instruction) == Opcode.CLL) {
+			// We need to translate identifier to address
+			// FIXME This will only work if call order is respected
+			final String callee = ctx.argument().getText();
+			// This must be fixed
+			if(this.classFile.getRoutinesTable().containsKey(callee)){
+				routine.getData().add(this.classFile.getRoutinesTable().get(callee).getId());
+			}else{
+				throw new RuntimeException("Unable to link unsorted call order (for now...)");
+			}
+		}
+		// Keep going
+		return super.visitInstruction(ctx);
+	}
+
+	@Override
+	public ClassFile visitRoutine(ArgoParser.RoutineContext ctx) {
+		// Identify routine name
+		final String name = ctx.label().name().getText();
+		// Create routine
+		final Routine routine = new Routine(name);
+		// Insert into mapper
+		this.classFile.getRoutinesTable().put(name, routine);
+		// Keep going
+		return super.visitRoutine(ctx);
+	}
+
+	@Override
+	public ClassFile visit(ParseTree tree) {
+		super.visit(tree);
+		return this.classFile;
+	}
+}
