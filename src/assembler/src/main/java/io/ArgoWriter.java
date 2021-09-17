fbf4a509539b5cf0abb039763604658fcfcea526
Giuseppe Baittiner <baittiner.giuseppe.dev@gmail.com>
2021-09-17 12:03:59 +0200

    Fixed invalid bytecode output (endianess)

--- src/assembler/src/main/java/io/ArgoWriter.java
+++ src/assembler/src/main/java/io/ArgoWriter.java
@@ -10,7 +10,7 @@ import java.nio.charset.StandardCharsets;
 
 public final class ArgoWriter {
 
-	public static final int[] SIGNATURE = { 0x41, 0x52, 0x47, 0x4f, 0x3c, 0x33, 0x4b, 0x53 };
+	public static final byte[] SIGNATURE = { 0x41, 0x52, 0x47, 0x4f, 0x3c, 0x33, 0x4b, 0x53 };
 
 	private final ClassFile classFile;
 
@@ -32,35 +32,42 @@ public final class ArgoWriter {
 	}
 
 	private void signature(FileOutputStream fos) throws IOException {
-		for (int i : SIGNATURE) { fos.write(i); }
+		for (byte i : SIGNATURE) { fos.write(i); }
 	}
 
 	private void version(FileOutputStream fos) throws IOException {
-		fos.write(Bytes.from(classFile.getMajorVersion()).resize(2).array());
-		fos.write(Bytes.from(classFile.getMinorVersion()).resize(2).array());
+		fos.write(asLittleEndian(classFile.getMajorVersion(), 2));
+		fos.write(asLittleEndian(classFile.getMinorVersion(), 2));
 	}
 
 	private void routine(FileOutputStream fos, Routine routine) throws IOException {
 		// Write id
-		fos.write(Bytes.from(routine.getId()).resize(2).array());
+		fos.write(asLittleEndian(routine.getId(), 2));
 		// Write name size
-		fos.write(routine.getName().getBytes(StandardCharsets.UTF_8).length);
+		fos.write(asLittleEndian(routine.getName().length(), 1));
 		// Write actual name
 		fos.write(routine.getName().getBytes(StandardCharsets.UTF_8));
 		// Write instruction size
-		fos.write(routine.getData().size());
+		fos.write(asLittleEndian(routine.getData().size()));
 		// Write instructions
-		for (Integer data : routine.getData()) { fos.write(data); }
+		for (Integer data : routine.getData()) { fos.write(asLittleEndian(data)); }
 	}
 
 	private void table(FileOutputStream fos) throws IOException {
 		// Write routine table size
-		fos.write(Bytes.from(classFile.getRoutinesTable().size()).resize(2).array());
+		fos.write(asLittleEndian(classFile.getRoutinesTable().size(), 2));
 		// Write each routine
-		for (Routine rtn : classFile.getRoutinesTable().values()) {
+		for (Routine rtn : classFile.getRoutines()) {
 			routine(fos, rtn);
 		}
 	}
 
+	private byte[] asLittleEndian(int raw, int bytes){
+		return Bytes.from(raw).resize(bytes).reverse().array();
+	}
+
+	private byte[] asLittleEndian(int raw){
+		return Bytes.from(raw).reverse().array();
+	}
 
 }
